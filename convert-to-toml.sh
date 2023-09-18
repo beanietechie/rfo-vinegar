2754a02373ec904e45be6eda13a1195e0856ff41
BeanieTechie <beanietechie@gmail.com>
2023-09-18 16:42:39 -0500

    Add notice about deprecation

--- convert-to-toml.sh
+++ convert-to-toml.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # Not great but it gets the job done.
 cat Main/* | sed "/^[}{]/d; s/^\s*\"//; s/\":\s*/ = /; s/,$//"
+
+echo
+echo "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
+echo "THIS IS THE OLD SCRIPT, USE OF IT IS NOT RECOMMENDED. USE setup.sh INSTEAD."
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
